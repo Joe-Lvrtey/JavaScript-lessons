68493c0e9b78e23c17179d4eb2d5a93310b0ae43
Joseph Kofi Leslie Lartey <lesliejoe2k2@gmail.com>
2026-06-12 23:57:06 +0000

    fixed typos | added inline comments

--- Mordern Js/Js/script-6.js
+++ Mordern Js/Js/script-6.js
@@ -2,48 +2,46 @@
 
 const x1 = 20;
 const x2 = 30;
-if ( x1 == 21) {
-    console.log(`${x1} is less than ${x2}`);
+if (x1 == 21) {
+  console.log(`${x1} is less than ${x2}`);
 } else {
-    console.log('This will never run');
+  console.log("This will never run");
 }
 
-if (x1) console.log('lets get it');
-else console.log('lets go');
+if (x1) console.log("lets get it");
+else console.log("lets go");
 
 // switch
 
-const d = new Date(2024,3,29,9,12,10)
-const month = d.getMonth()
-const day = d.getDate()
+const d = new Date(2024, 3, 29, 9, 12, 10);
+const month = d.getMonth();
+const day = d.getDate();
 
 switch (month) {
-    case 3:
-        console.log(`it is thee ${month}rd month`);
-        break
-    case 4:
-        console.log(`it is thee ${month}rd month`);
-    default:
-        break;
+  case 3: // month is 0 based, so 3 is april
+    console.log(`it is the ${month}rd month`);
+    break;
+  case 4:
+    console.log(`it is the ${month}th month`);
+  default:
+    break;
 }
 
-const calculator = (num1, num2, operator) =>{
-    if (operator == '+') {
-        return num1 + num2
-    } else if (operator == '-'){
-        return num1 - num2
-    }
-    else if (operator == '/'){
-        return num1 / num2
-    }
-    else if (operator == '*'){
-        return num1 * num2
-    } else{
-        return `please give a correct operator, ${operator} is not a math operator`
-    }
-}
+const calculator = (num1, num2, operator) => {
+  if (operator == "+") {
+    return num1 + num2;
+  } else if (operator == "-") {
+    return num1 - num2;
+  } else if (operator == "/") {
+    return num1 / num2;
+  } else if (operator == "*") {
+    return num1 * num2;
+  } else {
+    return `please give a correct operator, ${operator} is not a math operator`;
+  }
+};
 
-const answer = calculator(2,3,'/')
+const answer = calculator(2, 3, "/");
 console.log(Math.abs(answer));
 
 // truthy and falsy values
@@ -55,32 +53,31 @@ console.log(Math.abs(answer));
 // Nan
 // null
 
-// truthy anf falsy caveats
+// truthy and falsy caveats
 
 const children = 0;
 // since 0 is a falsy children will be cohersed into false and the falsy  statement will run
-if (children!=NaN){
-    console.log(`you have ${children} children`);
-}else{
-    console.log(`please enter the number of children you have`);
+if (children != NaN) {
+  console.log(`you have ${children} children`);
+} else {
+  console.log(`please enter the number of children you have`);
 }
 
 // checking for empty arrays
-const posts = [1]
-if (posts.length!=0) {
-    console.log('lists posts');
+const posts = [1];
+if (posts.length != 0) {
+  console.log("lists posts");
 } else {
-    console.log('no posts to list');
+  console.log("no posts to list");
 }
 
 // checking for empty objects
 
-const user1 = {
-}
-if (Object.keys(user1).length!== 0 ) {
-    console.log(`user 1 has a name of ${user1.name}`);
+const user1 = {};
+if (Object.keys(user1).length !== 0) {
+  console.log(`user 1 has a name of ${user1.name}`);
 } else {
-    console.log('user1 is an empty object');
+  console.log("user1 is an empty object");
 }
 
 // == vs ===
@@ -90,7 +87,7 @@ if (Object.keys(user1).length!== 0 ) {
 // logical operators
 
 // && true for all
-// || true for one -- either 
+// || true for one -- either
 
 // && returns the fisrt falsey value or the last value
 
@@ -108,14 +105,13 @@ b = 1 || 2 || 0;
 
 console.log(b);
 
-
 // ?? returns the right side operator when the left side is null or undefined
 
 let c;
 
 c == null;
 
-c = c?? 20;
+c = c ?? 20;
 console.log(c);
 
 // Ternary operator
@@ -123,12 +119,14 @@ console.log(c);
 const age = 9;
 
 // do this else that
-(age >= 18) ? console.log('You can vote') : console.log('You cannot vote until you are 18 buddy');
+age >= 18
+  ? console.log("You can vote")
+  : console.log("You cannot vote until you are 18 buddy");
 
 // assigning a conditional value to a variable
 
-const name_ = 'Flacko'
+const name_ = "Flacko";
 
-const userName = name_ == 'Flacko' ? true : console.log('Name is not Flacko');
+const userName = name_ == "Flacko" ? true : console.log("Name is not Flacko");
 
 console.log(userName);
